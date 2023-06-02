e00412564677a2571975a8e4cf5bae2d9630622a
annu-ust <annu.mathew1@ust.com>
2023-06-02 16:43:34 +0530

    first commit

---
+++ src/main/java/com/example/awspipeline/AwspipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.awspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwspipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwspipelineApplication.class, args);
+	}
+
+}
